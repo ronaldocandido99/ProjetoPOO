2d845ae1bc8d77b24d5ef3ae3086b27bc6dd97bd
Matheus Ramos <matheusr808@gmail.com>
2023-02-08 18:05:21 -0300

    Update Movimentacao.java

--- src/classe/Movimentacao.java
+++ src/classe/Movimentacao.java
@@ -40,8 +40,32 @@ public class Movimentacao{
         this.tipoProduto = tipoProduto;
     }
 
-    public int gettTipoProduto() {
-        return tipoProduto);
+    public int getTipoProduto() {
+        return idProduto;
+    }
+
+    public String getTipoProduto_string(){
+        int tipoProduto;
+        tipoProduto = getTipoProduto() 
+        if (tipoProduto == 1) {
+            return "Roupa Feminina";
+        } else if (tipoProduto == 2) {
+            return "Roupa Masculina";
+        } else if (tipoProduto == 3) {
+            return "Roupa Infantil";
+        } else if (tipoProduto == 4) {
+            return "Calçados";
+        }else if (tipoProduto == 5) {
+            return "Artigos de Casa";
+        }else if (tipoProduto == 6) {
+            return "Acessórios";
+        }else if (tipoProduto == 7) {
+            return "Eletrônicos";
+        }else if (tipoProduto == 8) {
+            return "Perfumes";
+        }else {
+            return "Tipo Invalido!!";
+        }
     }
 
     public void setDestino(String Destino){
@@ -73,7 +97,7 @@ public class Movimentacao{
         if (getId() != -1) {
             System.out.println("Data da Movimentação: " + getDataMovimentacao());
             System.out.println("Produto: " + gettTipoProduto(());
-            System.out.println("Categoria do Produto: " +  gettTipoProduto() );
+            System.out.println("Categoria do Produto: " +  getTipoProduto_string() );
             System.out.println("Responsavel da Movimentação: " + getIdResponsavel());
             System.out.println("Destino da Movimentação: " + getDestino());  
         } else {
@@ -81,6 +105,18 @@ public class Movimentacao{
         }
         System.out.println();
     }
+
+    public void CategoriaProduto() {// função de apoio
+
+        int aux1;
+        System.out.println("Informe a Categoria do Produto(somente numeros):");
+        System.out.println("Roupa Feminina(1);\n" + "Roupa Masculina(2);" + "Roupa Infantil(3);\n" 
+        + "Calçados(4);" + "Artigos de Casa;\n" + "Acessórios(6);" + "Eletrônicos(7);" + "Perfumes(8)");
+        aux1 = input.nextInt();
+        setTipoProduto(aux1);
+        input.nextLine();
+    }
+    
 
     public void add_m(int ficha) {// Adição de um empregado
 
@@ -96,10 +132,9 @@ public class Movimentacao{
         aux1 = input.nextInt();
         setIdProduto(aux1);
         input.nextLine();
-        System.out.println("Informe a Categoria do Produto(somente numeros): ");
-        aux1 = input.nextInt();
-        setTipoProduto(aux1);
-        input.nextLine();
+        //Categoria do Produto
+        CategoriaProduto()
+        //Categoria do Produto
         System.out.println("Informe O Destino do Produto: ");
         aux2 = input.nextLine();
         setDestino(aux2);
@@ -122,7 +157,7 @@ public class Movimentacao{
         + "Destino do Produto(4);" + "Origem do Produto(5);\n" + "Data da Movimentação do Produto(6);" + "Sair(7).");
     }
 
-    public void ed_e() {// Alterar detalhes de um empregado
+    public void ed_m() {// Alterar detalhes de um empregado
 
         int opcao = 1;
         long aux1;
@@ -149,10 +184,9 @@ public class Movimentacao{
                 System.out.println();
                 break;
             case 3:
-                System.out.println("Informe a Nova Categoria do Produto(somente numeros): ");
-                aux1 = input.nextLong();
-                setTipoProduto(aux1);
-                input.nextLine();
+                //Categoria do Produto
+                CategoriaProduto()
+                //Categoria do Produto
                 System.out.println();
                 break;
             case 4:
@@ -187,5 +221,19 @@ public class Movimentacao{
             }
         }
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
