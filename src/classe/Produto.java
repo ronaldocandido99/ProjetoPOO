a1575195a7d7e4df6ee5df2df57a49ca25cdadfa
lilianfabricio <lilianfabriciom@gmail.com>
2023-02-08 17:45:40 -0300

    Update Produto.java

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -1,6 +1,14 @@
+/*1) Permitir a criação e remoção de informações referentes aos produtos, movimentações e funcionários/responsáveis;
+2) Permitir a edição de informações referentes aos produtos, movimentações e funcionários/responsáveis;
+3) Permitir a associação de produto a uma localidade do estoque;
+4) Permitir a alteração do status
+• “Em estoque”, “Sem estoque”, “quantidade” etc.*/
+
 import java.util.Scanner;
 
-public class Produto{
+public class Produtos{
+
+    public int id;
     public String nome;
     public String tamanho;
     public String cor;
@@ -10,28 +18,253 @@ public class Produto{
     public String categoria;
     public String fabricante;
     public String fornecedor;
+    
+    Scanner input = new Scanner(System.in);
 
-    public Produto(String nome, String tamanho, String cor, String marca, int preco, String tipo, String categoria, String fabricante, String fornecedor){
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setTamanho(String tamanho) {
         this.tamanho = tamanho;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setCor(String cor) {
         this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setMarca(String marca) {
         this.marca = marca;
+    }
+
+    public String getMarca() {
+        return cpf;
+    }
+
+    public void setPreco(int preco){
         this.preco = preco;
+    }
+
+    public int getPreco(){
+        return preco;
+    }
+
+    public void setTipo(String tipo){
         this.tipo = tipo;
+    }
+    
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setCategoria(String categoria){
         this.categoria = categoria;
+    }
+
+    public int getCategoria(){
+        return categoria;
+    }
+
+    public void setFabricante(String fabricante){
         this.fabricante = fabricante;
+    }
+
+    public String getFabricante(){
+        return fabricante;
+    }
+
+    public void setFornecedor(String fornecedor){
         this.fornecedor = fornecedor;
     }
 
-    public String printProduto(){
-        return "Nome: " + this.nome +
-        "\nTamanho: " + this.tamanho +
-        "\nCor: " + this.cor +
-        "\nMarca: " + this.marca +
-        "\nPreco: " + this.preco +
-        "\nTipo: " + this.tipo +
-        "\nCategoria: " + this.categoria +
-        "\nFabricante: " + this.fabricante +
-        "\nFornecedor: " + this.fornecedor;
+    public String getFornecedor(){
+        return fornecedor;
     }
+
+
+    public void imp_e() {
+
+        if (getId() != -1) {
+            System.out.println("Identificação: " + getId());
+            System.out.println("Nome: " + getNome());
+            System.out.println("Email: " + getEmail());
+            System.out.println("Telefone: " + getTelefone());
+            System.out.println("CPF: " + getcpf());  
+        } else {
+            System.out.println("Empregado não existente");
+        }
+        System.out.println();
+    }
+
+    public void add_produto(int ficha) {// Adição de um produto
+
+        int aux1;
+        String aux2;
+
+        setId(ficha);
+        System.out.println("Informe o código de identificação do produto: ");
+        aux1 = input.nextLine();
+        setId(aux1);
+        System.out.println("Informe o Nome: ");
+        aux2 = input.nextLine();
+        setNome(aux2);
+        System.out.println("Informe o Tamanho: ");
+        aux1 = input.nextLong();
+        setTamanho(aux1);
+        System.out.println("Informe a cor: ");
+        aux2 = input.nextLine();
+        setCor(aux2);
+        System.out.println("Informe a marca: ");
+        aux2 = input.nextLine();
+        setMarca(aux2);
+        System.out.println("Informe o preco: ");
+        aux1 = input.nextLine();
+        setPreco(aux1);
+        System.out.println("Informe o tipo: ");
+        aux2 = input.nextLine();
+        setTipo(aux2);
+        System.out.println("Informe a categoria: ");
+        aux2 = input.nextLine();
+        setCategoria(aux2);
+        System.out.println("Informe o fabricante: ");
+        aux2 = input.nextLine();
+        setFabricante(aux2);
+        System.out.println("Informe o fornecedor: ");
+        aux2 = input.nextLine();
+        setFornecedor(aux2);
+        System.out.println();
+    }
+
+    public void remover_produto() {// Remoção de um produto
+
+        int aux1 = -1;
+        String aux2 = null;
+
+        setId(aux1);
+        setNome(aux2);
+        setTamanho(aux2);
+        setCor(aux2);
+        setMarca(aux2);
+        setPreco(aux1);
+        setTipo(aux2);
+        setCategoria(aux2);
+        setFabricante(aux2);
+        setFornecedor(aux2);
+    }
+
+    public void M_Inicial() {// função de apoio
+
+        System.out.println("Informe o campo que deseja alterar:");
+        System.out.println("ID(1);\n" + "Nome(2);\n" + "Tamanho(3);\n" + 
+        "Cor(4);\n" + "Marca(5);\n" + "Preço(6);\n" + "Tipo(7);\n" + "Categoria(8);\n" +
+        "Fabricante(9);\n" + "Fornecedor(10);\n"
+                + "Sair(11).");
+    }
+
+    public void editar_produto() {// Alterar detalhes de um empregado
+
+        int opcao = 1;
+        int aux1;
+        String aux2;
+
+        while (opcao != 11) {
+
+            M_Inicial();
+            opcao = input.nextInt();
+            switch (opcao) {
+            case 1:
+                System.out.println("Informe o Novo Id: ");
+                aux1 = input.nextInt();
+                setId(aux1);
+                input.nextLine();
+                System.out.println();
+                break;
+            case 2:
+                System.out.println("Informe o Novo Nome: ");
+                aux2 = input.nextLine();
+                setNome(aux2);
+                System.out.println();
+                break;
+            case 3:
+                System.out.println("Informe o Novo Tamanho: ");
+                aux2 = input.nextLine();
+                setTamanho(aux2);
+                System.out.println();
+                break;
+            case 4:
+                System.out.println("Informe a Nova Cor: ");
+                aux2 = input.nextLine();
+                setCor(aux2);
+                System.out.println();
+                break;
+            case 5:
+                System.out.println("Informe a Nova Marca: ");
+                aux2 = input.nextLine();
+                setMarca(aux2);
+                System.out.println();
+                break;
+            case 6:
+                System.out.println("Informe o Novo Preço: ");
+                aux1 = input.nextInt();
+                setPreco(aux1);
+                input.nextLine();
+                System.out.println();
+                break;
+            case 7:
+                System.out.println("Informe o Novo Tipo: ");
+                aux2 = input.nextLine();
+                setCor(aux2);
+                System.out.println();
+                break;
+            case 8:
+                System.out.println("Informe a Nova Categoria: ");
+                aux2 = input.nextLine();
+                setCategoria(aux2);
+                System.out.println();
+                break;
+            case 9:
+                System.out.println("Informe o Novo Fabricante: ");
+                aux2 = input.nextLine();
+                setFabricante(aux2);
+                System.out.println();
+                break;
+            case 10:
+                System.out.println("Informe o Novo Fornecedor: ");
+                aux2 = input.nextLine();
+                setFornecedor(aux2);
+                System.out.println();
+                break;
+
+            case 11:
+                System.out.println("Modificações Salvas!!");
+                System.out.println();
+                break;
+            default:
+                System.out.println("Opção Invalida!!!");
+                System.out.println("Digite Novamente");
+                System.out.println();
+            }
+        }
+    }
+
 }
